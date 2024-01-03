f9788753c6d4445fddc2fc06ba79d02e0daf0288
KristinaBychenok <kristinabychenokPR@gmail.com>
2024-01-03 20:31:38 +0100

    Add workflows

--- src/components/Sections/projects/projects.constants.ts
+++ src/components/Sections/projects/projects.constants.ts
@@ -1,3 +1,5 @@
+const { BASE_URL } = import.meta.env
+
 export const projects = [
   {
     name: 'Platform for booking of travel',
@@ -22,17 +24,17 @@ export const projects = [
     ],
     imgs: [
       {
-        src: './project-1.png',
+        src: `${BASE_URL}project-1.png`,
         text:
           "The 'Create/Edit Project' section features user-fillable fields related to projects and travelers. The 'SAVE' button activates upon correctly filling any field, while the 'SUBMIT' button activates only after all required fields are correctly filled. Both buttons send the data to the database. Users can reset all filled fields before saving, and saved data persists through project close and reopen. The fields include various types such as inputs, textareas, dropdowns, and checkboxes. Upon project creation, it is added to the database and displayed on the 'PROJECTS' page. An 'OVERVIEW' page provides projects statistics.",
       },
       {
-        src: './project-2.png',
+        src: `${BASE_URL}project-2.png`,
         text:
           'The second tab allows users to add travelers to their projects, displaying a table with added travelers. New travelers can be added, and existing ones can be added from the database using email. A drawer with editable information about the added traveler can be opened.',
       },
       {
-        src: './project-3.png',
+        src: `${BASE_URL}project-3.png`,
         text: '',
       },
     ],
--- src/components/Title/title.tsx
+++ src/components/Title/title.tsx
@@ -1,7 +1,9 @@
+const { BASE_URL } = import.meta.env
+
 export const Title = () => {
   return (
     <div className="flex flex-row md:max-lg:justify-center lg:max-xl:flex-col gap-[20px] items-center xl:gap-[30px] xl:items-start">
-      <img src="./main-img.png" className="lg:w-36" />
+      <img src={`${BASE_URL}main-img.png`} className="lg:w-36" />
       <div className="flex flex-col gap-[10px] w-64 items-start">
         <h1 className="font-custom text-5xl font-normal text-start 2xl:text-6xl xl:text-6xl">
           Kristina Bychenok
